d13d78f431f14e9a4139bc4cc7874f654daf4184
Jevade <liujiawei0524@gmail.com>
2018-08-27 17:17:51 +0800

    Init

---
+++ gofile/test/insert.go
@@ -0,0 +1,40 @@
+// package main
+
+// import (
+//             "fmt"
+//             "test"
+// )
+
+// func main() {
+//         a := insertSort(1, 2)
+//         fmt.Println(a)
+//         result, ok := mysqrt(1121)
+//         if ok {
+//                 fmt.Println(result)
+//         }
+//         fmt.Println(IsNumPosi(17.0))
+//         season, err := Season(3)
+//         if err {
+//                 fmt.Println("not in range")
+//                 return
+//         }
+//         fmt.Println(season)
+//         itera(7)
+//         iteraStr("This is a very import question!")
+//         gotointera(9)
+//         printIttera(10)
+//         printGoIttera(10)
+//         fmt.Println(MultiPly3Num(1,2,3))
+//         fmt.Println(add(1,2))
+//         fmt.Println(add_3(1,2))
+//         fmt.Println(sub(1,2))
+//         fmt.Println(sub_3(1,2))
+//         fmt.Println(multi(1,2))
+//         fmt.Println(multi_3(1,2))
+//         fmt.Println(mysqrts_3(123))
+// }
+
+
+
+
+
