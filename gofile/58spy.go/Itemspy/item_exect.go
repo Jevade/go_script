68a49a7ddc791708a76014bcb15320eeafa8bfa4
Jevade <liujiawei0524@gmail.com>
2019-03-26 14:52:29 +0800

    增加省份城市

--- gofile/58spy.go/Itemspy/item_exect.go
+++ gofile/58spy.go/Itemspy/item_exect.go
@@ -6,13 +6,16 @@ import (
 	"net/http"
 	"strconv"
 	"strings"
+	"time"
 
 	"../model"
 	"github.com/PuerkitoBio/goquery"
 )
 
+const baseFormat = "2006-01-02 15:04:05"
+
 //GetItemInfo is to get iteminfo from website
-func GetItemInfo(url string, itemch chan<- interface{}) {
+func GetItemInfo(province, city, url string, itemch chan<- interface{}) {
 
 	fmt.Println("Searching items")
 	res, _ := http.Get(url)
@@ -35,7 +38,8 @@ func GetItemInfo(url string, itemch chan<- interface{}) {
 	// Find the review items
 	doc.Find("div#infolist").Find("tr.ac_item").Each(func(j int, divSelection *goquery.Selection) {
 		fmt.Println(j)
-		itemInfo := &model.ItemInfo{}
+		nt := time.Now()
+		itemInfo := &model.ItemInfo{Province: province, City: city, UpdatedAt: nt.Format(baseFormat)}
 		itemInfo.Desc = RemoveSpace(divSelection.Find("div.item-desc").Text())
 		itemInfo.ItemTitle = RemoveSpace(divSelection.Find("td.t").Find("a").Eq(0).Text())
 		itemInfo.URL, _ = divSelection.Find("a").Eq(0).Attr("href")
